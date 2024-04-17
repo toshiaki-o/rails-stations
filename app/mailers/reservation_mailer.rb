31d82ca9eb08a41b6a4ae65d4ea31f8c8a6de0a6
toshiaki <104ak10kada@gmail.com>
2024-04-17 23:00:28 +0900

    定時実行対応

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -4,4 +4,9 @@ class ReservationMailer < ApplicationMailer
     @reservation = params[:reservation]
     mail(to: @reservation.email, subject: "予約完了メールです")
   end
+
+  def reminder
+    @reservation = params[:reservation]
+    mail(to: @reservation.email, subject: "予約前日の確認メールです")
+  end
 end
